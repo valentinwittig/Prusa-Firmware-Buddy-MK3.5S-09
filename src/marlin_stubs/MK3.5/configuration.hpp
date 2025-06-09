8874e2a213e2956cfd515a34ee14fb6a32fc718c
valentinwittig <info@printedbyvalentin.de>
2025-06-09 16:21:33 +0200

    Update configuration.hpp like MK4

--- src/marlin_stubs/MK3.5/configuration.hpp
+++ src/marlin_stubs/MK3.5/configuration.hpp
@@ -12,7 +12,7 @@ inline constexpr float axis_home_min_diff(uint8_t axis_num) {
     if (axis_num >= 3) {
         return NAN;
     }
-    float arr[] = { -0.2, -0.2, -0.1 };
+    float arr[] = { -0.1, -0.1, -0.1 };
     return arr[axis_num];
 }
 
