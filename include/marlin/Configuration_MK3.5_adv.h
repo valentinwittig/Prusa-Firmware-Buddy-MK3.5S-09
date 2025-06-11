0ed3568f58fcb09d93e988856b040de38ebcaf57
valentinwittig <info@printedbyvalentin.de>
2025-06-11 08:04:27 +0200

    Update Configuration_MK3.5_adv.h  XY_Stall_Sens_Min and Max like MK4

--- include/marlin/Configuration_MK3.5_adv.h
+++ include/marlin/Configuration_MK3.5_adv.h
@@ -1822,8 +1822,8 @@
     #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
         // The range of stallguard sensitivities to probe and calibrate
         // (the required sensitivity varies by motor)
-        #define XY_STALL_SENSITIVITY_MIN -2
-        #define XY_STALL_SENSITIVITY_MAX 2
+        #define XY_STALL_SENSITIVITY_MIN -7
+        #define XY_STALL_SENSITIVITY_MAX -2
 
         // Read from config. May be int16 max if uncalibrated, which is
         // then handled in the Crash_s class.
